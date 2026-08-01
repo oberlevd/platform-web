e7d7dab868b01ffefffc9996bbc6b1a64789efa7
Daniil Oberlev <oberlev.daniil@yandex.ru>
2026-08-02 01:08:56 +0300

    feat(components): add badge component (#63)

---
+++ app/components/ui/badge/Badge.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { BadgeVariants } from "./variants"
+import type { HTMLAttributes } from "vue"
+import { Primitive } from "reka-ui"
+import type { PrimitiveProps } from "reka-ui"
+
+import { badgeVariants } from "./variants"
+import { cn } from "@/lib/utils"
+import { reactiveOmit } from "@vueuse/core"
+
+const props = defineProps<PrimitiveProps & {
+  variant?: BadgeVariants["variant"]
+  class?: HTMLAttributes["class"]
+}>()
+
+const delegatedProps = reactiveOmit(props, "class")
+</script>
+
+<template>
+  <Primitive
+    data-slot="badge"
+    :class="cn(badgeVariants({ variant }), props.class)"
+    v-bind="delegatedProps"
+  >
+    <slot />
+  </Primitive>
+</template>
