7090ba18c3bcd4d4e0f597548713657b84ebd011
Daniil Oberlev <oberlev.daniil@yandex.ru>
2026-08-02 01:00:42 +0300

    feat(components): add button component

---
+++ app/components/ui/button/Button.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import type { ButtonVariants } from "./variants"
+import type { HTMLAttributes } from "vue"
+import { Primitive } from "reka-ui"
+import type { PrimitiveProps } from "reka-ui"
+
+import { buttonVariants } from "./variants"
+import { cn } from "~/lib/utils"
+
+interface Props extends PrimitiveProps {
+  variant?: ButtonVariants["variant"]
+  size?: ButtonVariants["size"]
+  class?: HTMLAttributes["class"]
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  as: "button",
+  asChild: false,
+  class: "",
+  size: "default",
+  variant: "default",
+})
+</script>
+
+<template>
+  <Primitive
+    data-slot="button"
+    :data-variant="variant"
+    :data-size="size"
+    :as="as"
+    :as-child="asChild"
+    :class="cn(buttonVariants({ variant, size }), props.class)"
+  >
+    <slot />
+  </Primitive>
+</template>
