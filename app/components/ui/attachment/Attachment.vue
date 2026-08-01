2d50484f0689a82a4cf34ebdb36f258a9f73e163
Daniil Oberlev <oberlev.daniil@yandex.ru>
2026-08-02 02:02:12 +0300

    feat(components): add attachment component (#70)

---
+++ app/components/ui/attachment/Attachment.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue"
+import type { AttachmentVariants } from "./variants"
+
+import { cn } from "@/lib/utils"
+import { attachmentVariants } from "./variants/index.ts";
+
+interface Props {
+  size?: AttachmentVariants["size"]
+  orientation?: AttachmentVariants["orientation"]
+  state?: "idle" | "uploading" | "processing" | "error" | "done"
+  class?: HTMLAttributes["class"]
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  orientation: "horizontal",
+  size: "default",
+  state: "done",
+})
+</script>
+
+<template>
+  <div
+    data-slot="attachment"
+    :data-state="state"
+    :data-size="size"
+    :data-orientation="orientation"
+    :class="cn(attachmentVariants({ size, orientation }), props.class)"
+  >
+    <slot />
+  </div>
+</template>
--- app/components/ui/button/index.ts
+++ app/components/ui/button/index.ts
@@ -1,1 +1,3 @@
+export type { ButtonVariants } from './variants.ts'
+
 export { default as Button } from "./Button.vue"
