a575dc0eb8c232bc453ff2e02afb8986c5d78158
Daniil Oberlev <oberlev.daniil@yandex.ru>
2026-08-02 01:41:09 +0300

    feat(components): add separator component
    N

---
+++ app/components/ui/separator/Separator.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { SeparatorProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import { Separator } from 'reka-ui'
+import { cn } from '@/lib/utils'
+
+const props = withDefaults(
+  defineProps<SeparatorProps & { class?: HTMLAttributes['class'] }>(),
+  {
+    decorative: true,
+    orientation: 'horizontal',
+  },
+)
+
+const delegatedProps = reactiveOmit(props, 'class')
+</script>
