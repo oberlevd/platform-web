b384dadf94813981a00a20cfa824d88134ceedd0
Daniil Oberlev <oberlev.daniil@yandex.ru>
2026-08-01 22:34:19 +0300

    chore(utils): add linter and formatter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  modules: ["@nuxt/ui", "oxc-nuxt"],
   css: ["./assets/css/main.css"], // относительно папки app
   vite: {
     plugins: [tailwindcss()],
