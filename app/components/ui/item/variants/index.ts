d75d70007568bd6ca5eea6012393aaf75985feef
Daniil Oberlev <oberlev.daniil@yandex.ru>
2026-08-04 18:34:14 +0300

    feat(components): add item component (#87)

---
+++ app/components/ui/item/variants/index.ts
@@ -0,0 +1,4 @@
+export type {ItemVariants} from './default-variants'
+export type {ItemMediaVariants} from './media-variants'
+export {itemVariants} from './default-variants'
+export {itemMediaVariants} from './media-variants'
---
+++ app/components/ui/item/variants/media-variants.ts
@@ -0,0 +1,21 @@
+import type { VariantProps } from "class-variance-authority"
+import { cva } from "class-variance-authority"
+
+export const itemMediaVariants = cva(
+  "flex shrink-0 items-center justify-center gap-2 group-has-[[data-slot=item-description]]/item:self-start [&_svg]:pointer-events-none group-has-[[data-slot=item-description]]/item:translate-y-0.5",
+  {
+    defaultVariants: {
+      variant: "default",
+    },
+    variants: {
+      variant: {
+        default: "bg-transparent",
+        icon: "size-8 border rounded-sm bg-muted [&_svg:not([class*='size-'])]:size-4",
+        image:
+          "size-10 rounded-sm overflow-hidden [&_img]:size-full [&_img]:object-cover",
+      },
+    },
+  },
+)
+
+export type ItemMediaVariants = VariantProps<typeof itemMediaVariants>
